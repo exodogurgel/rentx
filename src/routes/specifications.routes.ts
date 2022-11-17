5e31194472815af6940714beda272c5aefb8735a
Êxodo Gurgel <exodowellis@gmail.com>
2022-11-17 08:55:38 -0300

    :tada: Initial commit

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { CreateSpecificationsController } from "../modules/cars/useCases/createSpecifications/CreateSpecificationsController";
+
+const specificationsRoutes = Router();
+
+const createSpecificationsController = new CreateSpecificationsController();
+
+specificationsRoutes.post("/", createSpecificationsController.handle);
+
+export { specificationsRoutes };
