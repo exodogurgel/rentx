49c3eba501594d409c0382584abd0ee9769845c3
Êxodo Gurgel <exodowellis@gmail.com>
2022-11-18 18:05:23 -0300

    :sparkles: feat: Create middleware admin

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { CreateSpecificationsController } from "../../../../modules/cars/useCases/createSpecifications/CreateSpecificationsController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAutheticated";
 
 const specificationsRoutes = Router();
 
 const createSpecificationsController = new CreateSpecificationsController();
 
-specificationsRoutes.post("/", createSpecificationsController.handle);
+specificationsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationsController.handle
+);
 
 export { specificationsRoutes };
