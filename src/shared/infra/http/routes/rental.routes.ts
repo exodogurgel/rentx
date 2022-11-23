c3e12ab5589735f73d7baa32b895b1998ae5a00a
Êxodo Gurgel <exodowellis@gmail.com>
2022-11-23 10:49:35 -0300

    :sparkles: feat: Create car rental listing

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { CreateRentalController } from "../../../../modules/rentals/useCases/createRental/CreateRentalController";
 import { DevolutionRentalController } from "../../../../modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "../../../../modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 import { ensureAuthenticated } from "../middlewares/ensureAutheticated";
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
 
@@ -14,6 +16,12 @@ rentalRoutes.post(
   "/devolution/:id",
   ensureAuthenticated,
   devolutionRentalController.handle
+);
+
+rentalRoutes.get(
+  "/user",
+  ensureAuthenticated,
+  listRentalsByUserController.handle
 );
 
 export { rentalRoutes };
