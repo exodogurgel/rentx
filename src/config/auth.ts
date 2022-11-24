a241f0fbc86f174cfe2aaf6b9f0c837b28856c2d
Êxodo Gurgel <exodowellis@gmail.com>
2022-11-23 22:20:01 -0300

    :recycle: refactor: User authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "5fef3f99833bc4850e4cb6200ff09426",
+  expires_in_token: "15m",
+  secret_refresh_token: "cf6117e8a191192f9e518868beeaedea",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
